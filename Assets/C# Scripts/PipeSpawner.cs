26f3a439cc9db611ad459c8d0976dc1c0d983509
Furidamu <vycheslaveltex@gmail.com>
2021-07-02 21:15:41 +0700

    Fix bug

---
+++ Assets/C# Scripts/PipeSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    public GameObject Pipes;        // переменная для префабов 
+
+    void Start()
+    {
+        StartCoroutine(Spawner());  // включаем корутину "Spawner" 
+    }
+
+    IEnumerator Spawner()          
+    {
+        while (true)                // бесконечный цикл While - работает
+        {
+            yield return new WaitForSeconds(2);     // ждем 2 секунды
+            float rand = Random.Range(-1f, 4f);     // рандомная позиция от -1 до 4 (чтоб удобнее было)
+            GameObject newPipes = Instantiate(Pipes, new Vector3(2, rand, 0), Quaternion.identity);     // переносим отвественность на новый gameObject и создаем префаб
+            Destroy(newPipes, 10);  // удаление нового gameObject'a через 10 секунд (если б удаляли Pipes)
+        }
+    }
+}
---
+++ Assets/C# Scripts/Scene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class Scene : MonoBehaviour
+{
+    public void Restartlevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // перезагрузка сцены (но это я чет запарился)
+
+        // можно было написать вот так: SceneManager.LoadScene(0) - тот же эффект
+    }
+
+}
